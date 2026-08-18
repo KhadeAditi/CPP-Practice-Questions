b2f89483d31e6687fb76944588e61a72f2d8e87f
KhadeAditi <aditikhade0@gmail.com>
2026-08-18 15:49:50 +0000

    if-else

---
+++ 8.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number;
+    cout<<"enter a number: ";
+    cin>>number;
+    if(number%2==0){
+        cout<<"The number is even.";
+    }
+    else{
+        cout<<"the number is odd.";
+    }
+return 0;
+}
