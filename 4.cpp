7345bea2013a74b5ae30d495da3db8d7e1e72a64
KhadeAditi <aditikhade0@gmail.com>
2026-08-18 15:15:50 +0000

    area & perimeter

---
+++ 4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int length,breadth;
+
+    cout<<"Enter length and breadth of Rectangle :";
+    cin>>length>>breadth;
+
+    cout<<"Area of Rectangle is :"<<length*breadth<<endl;
+
+    cout<<"perimeter of Rectangle is :"<<2*(length + breadth)<<endl;
+
+return 0;
+}
