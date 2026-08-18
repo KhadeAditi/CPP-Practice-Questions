1857db9d06ec91a439fa8ebf36c629d032e237a6
KhadeAditi <aditikhade0@gmail.com>
2026-08-18 15:20:40 +0000

    area & circumference

---
+++ 5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    double radius;
+    
+    cout<<"Enter the radius of circle : ";
+    cin>>radius;
+
+    cout<<" Area of circle is :"<<3.14*radius*radius<<endl;
+
+    cout<<" circumference of circle is :"<<2*3.14*radius<<endl;
+    return 0;
+}
