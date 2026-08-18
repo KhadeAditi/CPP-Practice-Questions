d02c4e6299493cabbace922c92536c12d2d5322a
KhadeAditi <aditikhade0@gmail.com>
2026-08-18 15:28:06 +0000

    swapping using third variable

---
+++ 6.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,temp;
+    cout<<"enter two numbers :";
+    cin>>a>>b;
+    cout<<"before swapping a = "<<a<<"b = "<<b<<endl;
+    temp = a;
+    a=b;
+    b=temp;
+    cout<<"after swapping a ="<<a<<"b = "<<b<<endl;
+    return 0;
+}
